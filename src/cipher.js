9262a299af0ca597fca1d44b50d9bbf876bf454d
karinmelissa <karin.escobar@usach.cl>
2021-02-11 13:38:37 -0300

    cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
 const cipher = {
   encode : function(offset, string){
     var txt = "";
-    for (var i=0; i<string.length;i++){
+    for (let i=0; i<string.length;i++){
       let valor = string.charCodeAt(i);
       if (valor >= 65 && valor <= 90) {
         txt += String.fromCharCode((valor - 65 + parseInt(offset)) % 26 + 65);
@@ -15,7 +15,7 @@ const cipher = {
   },
   decode : function (offset, string){
     var txt = "";
-    for (var i=0; i<string.length;i++){
+    for (let i=0; i<string.length;i++){
       let valor = string.charCodeAt(i);
       if (valor >= 65 && valor <= 90) {
         txt += String.fromCharCode((valor - 90 + parseInt(offset)) % 26 + 90);
--- src/index.js
+++ src/index.js
@@ -1,37 +1,41 @@
 import cipher from './cipher.js';
 
-//document.getElementById('final').style.display = 'none';
 const desplazamiento = document.getElementById("desplazamiento");
 const texto = document.getElementById("texto");
 const textoCifrado = document.getElementById("cifrado");
 const click = document.getElementById("click");
 const show = document.getElementById("eye-show");
+const mensajeDefault = document.getElementById("saludo");
+const showInfo = document.getElementById("info-show");
+const hiddeInfo = document.getElementById("info-out");
 
-function cifrado(){
-    const textoIngresado = texto.value;
-    const valorDesplazamiento = parseInt(desplazamiento.value);
-    if(valorDesplazamiento>0){
-      const textoCifrado = document.getElementById("cifrado");
-      textoCifrado.value = cipher.encode(valorDesplazamiento, textoIngresado);
-      document.getElementById("cifrado").innerHTML = textoCifrado.value;
-    }
-    else{
-      textoCifrado.value = cipher.decode(valorDesplazamiento, textoIngresado);
-      document.getElementById("cifrado").innerHTML = textoCifrado.value;
-    }
+function cifrado() {
+  mensajeDefault.value = "Krod frpr hvwdv?";
+  document.getElementById("saludo").innerHTML = mensajeDefault.value;
+  const textoIngresado = texto.value;
+  const valorDesplazamiento = parseInt(desplazamiento.value);
+  if (valorDesplazamiento > 0) {
+    const textoCifrado = document.getElementById("cifrado");
+    textoCifrado.value = cipher.encode(valorDesplazamiento, textoIngresado);
+    document.getElementById("cifrado").innerHTML = textoCifrado.value;
+  }
+  else {
+    textoCifrado.value = cipher.decode(valorDesplazamiento, textoIngresado);
+    document.getElementById("cifrado").innerHTML = textoCifrado.value;
+  }
 }
 
-function desifrar(){
+function desifrar() {
   const textoADecifrar = document.getElementById("cifrado");
   const textoDecifrado = textoADecifrar.value;
   const valorDesplazamiento = - parseInt(desplazamiento.value);
   textoADecifrar.value = cipher.decode(valorDesplazamiento, textoDecifrado);
   document.getElementById("cifrado").innerHTML = textoADecifrar.value;
-  console.log(textoADecifrar);
-  console.log(textoDecifrado);
+  mensajeDefault.value = cipher.decode(-3, mensajeDefault.value);
+  document.getElementById("saludo").innerHTML = mensajeDefault.value;
 }
 
-function mostrar(){
+function mostrar() {
   const textoIngresado = texto.value;
   if (textoIngresado == '') {
     alert('Olvidaste ingresar tu mensaje');
@@ -40,19 +44,31 @@ function mostrar(){
     if (document.getElementById('burbuja').style.display == 'none') {
       document.getElementById('burbuja').style.display = 'flex';
     }
-    else{
+    else {
       document.getElementById('burbuja').style.display = 'flex';
-
     }
   }
 }
+function mostrarInfo(){
+  if(document.getElementById("informacion").style.display = 'none'){
+    document.getElementById('informacion').style.display = 'block';
+  }
+  else{
+    document.getElementById("informacion").style.display = 'none';
+  }
+}
+function ocultarInfo(){
+  if(document.getElementById("informacion").style.display = 'block'){
+    document.getElementById('informacion').style.display = 'none';
+  }
+  else{
+    document.getElementById("informacion").style.display = 'none';
+  }
+}
 
-//texto.addEventListener("keyup",cifrado);
-click.addEventListener("click",mostrar);
-click.addEventListener("click",cifrado);
-show.addEventListener("click",desifrar);
-
-//desplazamiento.addEventListener("change", cifrado);
-
-
-//console.log(cipher);
+click.addEventListener("click", mostrar);
+click.addEventListener("click", cifrado);
+show.addEventListener("mousedown", desifrar);
+show.addEventListener("mouseup", cifrado);
+showInfo.addEventListener("click",mostrarInfo);
+hiddeInfo.addEventListener("click",ocultarInfo);
